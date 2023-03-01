7ab45cc997c2837a0a27851d867d4329178a0f4c
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-03-01 12:45:22 +0500

    Lesson12 - contract as dependency

--- counting-contract/src/lib.rs
+++ counting-contract/src/lib.rs
@@ -1,7 +1,6 @@
-// Import various items from the `cosmwasm_std` library
-use cosmwasm_std::{
-    entry_point, to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
-};
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 use error::ContractError;
 use msg::InstantiateMsg;
 
@@ -9,12 +8,14 @@ use msg::InstantiateMsg;
 mod contract;
 pub mod error;
 pub mod msg;
-#[cfg(test)]
+#[cfg(any(test, feature = "tests"))]
 pub mod multitest;
 mod state;
 
 // Define the `instantiate` entry point function, which is called when a new contract is deployed to the blockchain
-#[entry_point]
+// This attribute is used to mark the function as an entry point for the smart contract.
+// It is conditionally compiled with a feature flag to prevent it from being included in the library version of the code.
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
     _env: Env,
@@ -25,7 +26,9 @@ pub fn instantiate(
 }
 
 // Define the `query` entry point function, which is called when a read-only operation is performed on the contract
-#[entry_point]
+// This attribute is used to mark the function as an entry point for the smart contract.
+// It is conditionally compiled with a feature flag to prevent it from being included in the library version of the code.
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
     // Import the `query` function from the `contract` module and the `QueryMsg` enum variants from the `msg` module
     use contract::query;
@@ -37,8 +40,11 @@ pub fn query(deps: Deps, _env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
         Value {} => to_binary(&query::value(deps)?),
     }
 }
+
 // Define the `execute` entry point function, which is called when a write operation is performed on the contract
-#[entry_point]
+// This attribute is used to mark the function as an entry point for the smart contract.
+// It is conditionally compiled with a feature flag to prevent it from being included in the library version of the code.
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
     env: Env,
